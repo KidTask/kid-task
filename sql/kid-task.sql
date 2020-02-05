3350725bf5a8fd9e1d7afd5262e72aab1e03e008
Gabriel Town <rieltown@gmail.com>
2020-02-05 13:47:24 -0700

    updated DDL added parentName and changed taskIsComplete to tinyint

--- sql/kid-task.sql
+++ sql/kid-task.sql
@@ -1,22 +1,25 @@
 CREATE TABLE parent (
 	parentId binary(16) not null,
 	parentActivationToken char(32),
-	parentUsername varchar(32) not null,
 	parentEmail varchar(128) not null,
 	parentHash char(96) not null,
+	parentName nvarchar(255),
+	parentUsername varchar(32) not null,
 	unique (parentEmail),
 	unique(parentUsername),
+	index(parentEmail),
 	primary key(parentId)
 );
 
 CREATE TABLE kid (
 	kidId binary(16) not null,
 	kidParentId binary(16) not null,
-	kidUsername varchar(32) not null,
-	kidName nvarchar(255),
 	kidAvatarUrl varchar(255),
 	kidHash char(96) not null,
+	kidName nvarchar(255),
+	kidUsername varchar(32) not null,
 	unique (kidUsername),
+	index(kidParentId),
 	foreign key(kidParentId) references parent(parentId),
 	primary key(kidId)
 );
@@ -27,8 +30,10 @@ CREATE TABLE task (
 	taskKidId binary(16) not null,
 	taskContent varchar(1000),
 	taskDueDate DATETIME(6),
-	taskIsComplete boolean,
+	taskIsComplete TINYINT,
 	taskReward varchar(255),
+	index(taskParentId),
+	index (taskKidId),
 	foreign key(taskParentId) references parent(parentId),
 	foreign key(taskKidId) references kid(kidId),
 	primary key(taskId)
@@ -37,8 +42,9 @@ CREATE TABLE task (
 CREATE TABLE step (
 	stepId binary(16) not null,
 	stepTaskId binary(16) not null,
-	stepOrder SMALLINT,
 	stepContent varchar(1000),
+	stepOrder SMALLINT,
+	index(stepTaskId),
 	foreign key(stepTaskId) references task(taskId),
 	primary key(stepId)
 );
