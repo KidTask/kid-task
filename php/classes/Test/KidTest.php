5fe486314344075e3f7f31e7c7596de310747a48
Jacob Lott <58454146+jlott1999@users.noreply.github.com>
2020-02-13 16:37:23 -0700

    added to KidTest.php

---
+++ php/classes/Test/KidTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Club\KidTask;
+use Ramsey\Uuid\uuid;
+
+require_once(dirname(__DIR__) . "/autoload.php");
+
+require_once(dirname(__DIR__) . "/lib/uuid.php");
+
+/**
+ * unit test for the Kid Class
+ * PDO methods are located in the Kid Class
+ * @ see php/classes/Kid.php
+ * @author Jacob Lott
+ */
+
