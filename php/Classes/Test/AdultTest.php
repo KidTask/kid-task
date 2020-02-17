2f6b1d811cf02f734eca832c24bf5b072d5ee69e
Gabriel Town <rieltown@gmail.com>
2020-02-17 15:46:37 -0700

    changed email again to figure out merge

--- php/Classes/Test/AdultTest.php
+++ php/Classes/Test/AdultTest.php
@@ -41,7 +41,7 @@ class AdultTest extends KidTaskTest {
 	 * valid email to use
 	 * @var string $VALID_EMAIL
 	 **/
-	protected $VALID_EMAIL = "bad@gmail.com";
+	protected $VALID_EMAIL = "fuj@gmail.com";
 
 	/**
 	 * valid hash to use
