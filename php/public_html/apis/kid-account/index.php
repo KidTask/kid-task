ed39ba5fc538d28eb46ee07ca215771202d15e2e
Jacob Lott <58454146+jlott1999@users.noreply.github.com>
2020-03-04 16:01:38 -0700

    Edited Kid Account

--- php/public_html/apis/kid-account/index.php
+++ php/public_html/apis/kid-account/index.php
@@ -30,7 +30,7 @@ $reply->data = null;
 try {
     //grab the mySQL connection
 
-    $secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
+    $secrets = new \Secrets("/etc/apache2/capstone-mysql/kidtask.ini");
     $pdo = $secrets->getPdoObject();
 
 
@@ -40,19 +40,35 @@ try {
     // sanitize input
     $kidId = filter_input(INPUT_GET, "kidId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
     $kidAdultId = filter_input(INPUT_GET, "kidAdultId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $kidAvatarUrl= filter_input(INPUT_GET, "kidAvatarUrl", FILTER_SANITIZE_URL);
+    $kidCloudinaryToken= filter_input(INPUT_GET, "kidCloudinaryToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $kidName = filter_input(INPUT_GET, "kidName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
     $kidUsername = filter_input(INPUT_GET, "kidUsername", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
+    //make sure avatar url is valid (optional field)
+        if (empty($requestObject->kidAvatarUrl) === true) {
+            $requestObject->kidAvatarUrl = null;
+        }
 
-    // make sure the id is valid for methods that require it
-    if(($method === "DELETE" || $method === "PUT") && (empty($kidId) === true)) {
-        throw(new InvalidArgumentException("Kid Id cannot be empty or negative", 405));
-    }
+        //make sure cloudinary token is valid (optional field)
+        if (empty($requestObject->kidCloudinaryToken) === true) {
+            $requestObject->kidCloudinaryToken = null;
+        }
+
+        //make sure name is valid (optional field)
+        if (empty($requestObject->kidName) === true) {
+            $requestObject->kidName = null;
+        }
+        // make sure the id is valid for methods that require it
+        if(($method === "DELETE" || $method === "PUT") && (empty($kidId) === true)) {
+            throw(new InvalidArgumentException("Kid Id cannot be empty or negative", 405));
+        }
 
     if($method === "GET") {
         //set XSRF cookie
         setXsrfCookie();
 
-        //gets a post by content
+        //gets a kid by content
         if(empty($kidId) === false) {
             $reply->data = Kid::getKidByKidId($pdo, $kidId);
 
@@ -70,7 +86,7 @@ try {
         verifyXsrf();
 
         //enforce the end user has a JWT token
-        //validateJwtHeader();
+        validateJwtHeader();
 
         //enforce the user is signed in and only trying to edit their own profile
         if(empty($_SESSION["kid"]) === true || $_SESSION["kid"]->getKidId()->toString() !== $kidId) {
@@ -83,26 +99,43 @@ try {
         $requestContent = file_get_contents("php://input");
         $requestObject = json_decode($requestContent);
 
-        //retrieve the profile to be updated
+        //retrieve the kid to be updated
         $kid = Kid::getKidByKidId($pdo, $kidId);
         if($kid === null) {
-            throw(new RuntimeException("Profile does not exist", 404));
+            throw(new RuntimeException("Profile does not exist.", 404));
         }
 
-
-        //profile at handle
+        //the kid's adult id
         $kid = Kid::getKidByKidAdultId($pdo, $kidAdultId);
         if($kid === null) {
-            throw(new RuntimeException("Profile does not exist", 404));
+            throw(new RuntimeException("Profile does not exist.", 404));
+        }
+
+        //kid's avatar url
+        if(empty($requestObject->kidAvatarUrl) === true) {
+            throw(new \InvalidArgumentException("No Kid Avatar Url is present.", 405));
+        }
+
+        //kid's cloudinary token
+        if(empty($requestObject->kidCloudinaryToken) === true) {
+            throw(new \InvalidArgumentException("No Cloudinary Token for Kid.", 405));
+        }
+
+        //Kid Name
+        if(empty($requestObject->kidName) === true) {
+            throw(new \InvalidArgumentException("No kid name!", 405));
         }
 
         //Kid Username
         if(empty($requestObject->kidUsername) === true) {
-            throw(new \InvalidArgumentException ("No Kid Username present", 405));
+            throw(new \InvalidArgumentException ("No Kid Username present.", 405));
         }
 
 
         $kid->setKidAdultId($requestObject->kidAdultId);
+        $kid->setKidAvatarUrl($requestObject->kidAvatarUrl);
+        $kid->setKidCloudinaryToken($requestObject->kidCloudinaryToken);
+        $kid->setKidName($requestObject->kidName);
         $kid->setKidUsername($requestObject->kidUsername);
         $kid->update($pdo);
 
