8e74673836748b2de60b9cc993c449fd9bce8e57
Demetria Gutierrez <accounts@calyx.studio>
2020-03-09 15:20:49 -0600

    Added back Footer.js; style.css + updated Home.js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from "react"
+// import {Header} from "../shared/components/header/Header";
+import {Footer} from "../shared/components/footer/Footer";
+
 
 export const Home = () => {
 	return (
 		<>
-		<h1>Home</h1>
+			<div className="container">
+				<div className="row">
+					{/*<Header/>*/}
+				</div>
+				<div className="row">
+					<div className="col-lg-8">
+						<h1>Home</h1>
+					</div>
+				</div>
+				<div className="row">
+					<Footer/>
+				</div>
+			</div>
 		</>
-)
+	)
 }
---
+++ app/src/shared/components/footer/Footer.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import {Link} from "react-router-dom";
+
+
+import "../../../style.css";
+
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faInfoCircle, faUserFriends} from "@fortawesome/free-solid-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faInfoCircle, faGithub, faEnvelope, faUserFriends);
+
+export const Footer = () => (
+	<>
+		<footer className="page-footer bg-light fixed-bottom py-2">
+			<Container className="container">
+				<div className="d-flex justify-content-center">
+					<Row id="icons">
+						<Col>
+							<Link to='/about'>
+								<i><FontAwesomeIcon icon={faInfoCircle} size="sm" alt="Kid Task Project Information"/></i>
+							</Link>
+						</Col>
+						<Col>
+							<a href="https://github.com/KidTask/kid-task"
+								title="KidTask' repository on GitHub">
+								<i><FontAwesomeIcon icon={faGithub} size="sm"/></i>
+							</a>
+						</Col>
+						<Col>
+							<a href="mailto:kidtask@gmail.com"
+								title="Kid Task's email">
+								<i><FontAwesomeIcon icon={faEnvelope} size="sm"/></i>
+							</a>
+						</Col>
+						<Col>
+							<Link to='/team'>
+								<i><FontAwesomeIcon icon={faUserFriends} size="sm" alt="Kid Task Team"/></i>
+							</Link>
+						</Col>
+					</Row>
+				</div>
+				<div className="d-flex justify-content-center">
+					<Row>
+						<Col id="group-name">&reg; 2020 By Kid Task – CNM Ingenuity Deep Dive Bootcamp Group</Col>
+					</Row>
+				</div>
+			</Container>
+		</footer>
+	</>
+);
