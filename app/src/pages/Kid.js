d4a55a744a864a3707ec4e1cf3fe2ee114df7ed4
Demetria Gutierrez <accounts@calyx.studio>
2020-03-10 14:23:07 -0600

    Kid.js as the kid dashboard - removed artifact.

--- app/src/pages/Kid.js
+++ app/src/pages/Kid.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Header} from "../shared/components/header/Header";
 import {Footer} from "../shared/components/footer/Footer";
-import {LoginFormContent} from "../shared/components/login/LoginFormContent";
+
 
 //REACT BOOTSTRAP CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,16 +14,7 @@ export const Home = () => {
 					<Header/>
 				</div>
 				<div className="row">
-
-
-
-
-
-								<Tasks/>
-
-
-
-
+					<Tasks/>
 				</div>
 				<div className="row">
 					<Footer/>
