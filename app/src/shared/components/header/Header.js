7ec0eb0f41d34972ec84a519d67452140afcf84d
Gabriel Town <rieltown@gmail.com>
2020-03-12 15:53:36 -0600

    changed header to add menu on right

--- app/src/shared/components/header/Header.js
+++ app/src/shared/components/header/Header.js
@@ -2,12 +2,33 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import "../../../style.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from 'react-bootstrap/Image'
+
+
 export const Header = (props) => {
     return(
-        <Navbar expand="lg" variant="light" bg="light">
-            <Container>
-                <Navbar.Brand href="#">Kid Task</Navbar.Brand>
-            </Container>
+        <Navbar variant="light" bg="light">
+           <Container>
+              <Navbar.Brand href="#">Kid Task</Navbar.Brand>
+              <Navbar.Collapse className="justify-content-end">
+                 <NavDropdown alignRight title="" id="dropdown-menu-align-right">
+                    <NavDropdown.Item href="#action/3.1">+ Add Kid</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.2">Edit Profile</NavDropdown.Item>
+                 </NavDropdown>
+              </Navbar.Collapse>
+              <div className="pull-right">
+                 <Image className="thumbnail-image"
+                        src="http://www.fillmurray.com/200/300"
+                        width="30"
+                        height="30"
+                        alt="user pic"
+                        rounded
+                 />
+              </div>
+           </Container>
+
         </Navbar>
 )
 };
