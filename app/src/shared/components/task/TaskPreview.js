a8dcd93b5a0108866097a7ba0b03efcc9209a297
Demetria Gutierrez <accounts@calyx.studio>
2020-03-13 08:53:35 -0600

    created step component; added titles and styling

--- app/src/shared/components/task/TaskPreview.js
+++ app/src/shared/components/task/TaskPreview.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import {StepPreview} from "../step/StepPreview";
 
 
 export const TaskPreview = () => {
@@ -11,22 +12,20 @@ export const TaskPreview = () => {
 			<div className="col-md-5 col-lg-4 mx-auto mb-5">
 				<Card border="info">
 					<Card.Header>
-						<ProgressBar animated now={33} variant="info" label={`Working on it!`} />
+						<h3 className="title">Due soon</h3>
+						<ProgressBar animated now={33} variant="info" label={`Working on it!`}/>
 					</Card.Header>
-					<Card.Body className="row">
-						<div>
-						<img className="taskAvatar mx-2" alt="task avatar"
-							  src="https://images.unsplash.com/photo-1524420533980-5fe0aecc5fe6"/></div>
-			<div className="col-9">
-						<Card.Title>This is where the task would be if it were a run on sentence that would be very complicated for a young kid to grasp.</Card.Title>
-		</div>
-				</Card.Body>
-					<ListGroup variant="flush">
-						<ListGroup.Item>This would be step 1 after clicking the Begin Task button</ListGroup.Item>
-						<ListGroup.Item>This would be step 2 </ListGroup.Item>
-						<ListGroup.Item>This would be step 3 </ListGroup.Item>
 
-					</ListGroup>
+
+					<Card.Img variant="top" src="https://images.unsplash.com/photo-1524420533980-5fe0aecc5fe6"/>
+					<Card.Body >
+						<h3 className="title">Task</h3>
+						<Card.Title className="kidCardTitle">This is where the task would be.</Card.Title>
+					</Card.Body>
+					<Card.Body>
+						<h3 className="title">Steps</h3>
+						<StepPreview/>
+					</Card.Body>
 					<ListGroup variant="flush" className="beginTask">
 						<ListGroup.Item><Button variant="outline-info">Begin task</Button></ListGroup.Item>
 					</ListGroup>
