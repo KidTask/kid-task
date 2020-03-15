8e138e5a2a1696ecaef0ce67a7567576c2bd046f
Demetria Gutierrez <accounts@calyx.studio>
2020-03-15 13:35:30 -0600

    Created actions: kid-account-actions.js, step-actions.js, task-actions.js. Updated sign-in-actions.js

---
+++ app/src/shared/actions/kid-account-actions.js
@@ -0,0 +1,26 @@
+import { httpConfig } from '../utils/http-config'
+
+export const getKidByKidId = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/sign-in/?kidId=${kidId}')
+	dispatch({ type: 'GET_KID_BY_KID_ID', payload: data })
+};
+
+export const getKidByKidAdultId = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/sign-in/?kidAdultId=${kidAdultId}')
+	dispatch({ type: 'GET_KID_BY_KID_ADULT_ID', payload: data })
+};
+
+export const getKidUsername = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/sign-in/?kidUsername=${kidUsername}')
+	dispatch({ type: 'GET_KID_USERNAME', payload: data })
+};
+
+export const getKidAvatarUrl = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/sign-in/?kidAvatarUrl=${kidAvatarUrl}')
+	dispatch({ type: 'GET_KID_AVATAR_URL', payload: data })
+};
+
+export const getKidName = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/sign-in/?kidName=${kidName}')
+	dispatch({ type: 'GET_KID_NAME', payload: data })
+};
--- app/src/shared/actions/sign-in-actions.js
+++ app/src/shared/actions/sign-in-actions.js
@@ -1,21 +1,21 @@
 import { httpConfig } from '../utils/http-config'
 
 export const getAdultByAdultUsername = () => async (dispatch) => {
-	const {data} = await httpConfig('/apis/sign-in/?adultUsername')
+	const {data} = await httpConfig('/apis/sign-in/?adultUsername=${adultUsername}')
 	dispatch({ type: 'GET_ADULT_BY_ADULT_USERNAME', payload: data })
 };
 
 export const getAdultByAdultId = () => async (dispatch) => {
-	const {data} = await httpConfig('/apis/sign-in/?')
+	const {data} = await httpConfig('/apis/sign-in/?${adultId}')
 	dispatch({ type: 'GET_ADULT_BY_ADULT_ID', payload: data })
 };
 
 export const getKidByKidUsername = () => async (dispatch) => {
-	const {data} = await httpConfig('/apis/sign-in/?')
+	const {data} = await httpConfig('/apis/sign-in/?kidUsername=${kidUsername}')
 	dispatch({ type: 'GET_KID_BY_KID_USERNAME', payload: data })
 };
 
 export const getKidByKidId = () => async (dispatch) => {
-	const {data} = await httpConfig('/apis/sign-in/?')
+	const {data} = await httpConfig('/apis/sign-in/${kidId}')
 	dispatch({ type: 'GET_KID_BY_KID_ID', payload: data })
 };
