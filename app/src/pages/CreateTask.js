1e2c6e351e1061b227eb80719abc3ab88567a816
Gabriel Town <rieltown@gmail.com>
2020-03-16 20:15:26 -0600

    created task form

---
+++ app/src/pages/CreateTask.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Header} from "../shared/components/header/Header";
+import {Footer} from "../shared/components/footer/Footer";
+import
+
+
+export const AdultDashboard = () => {
+
+
+}
--- app/src/shared/components/taskform/TaskForm.js
+++ app/src/shared/components/taskform/TaskForm.js
@@ -1,78 +1,94 @@
 import React from 'react';
-import { Formik } from 'formik';
+import {ErrorMessage, Field, FieldArray, Formik} from 'formik';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import * as yup from 'yup';
+import * as Yup from 'yup';
 import { addTask, getTasks } from './requests';
 import { connect } from 'react-redux';
 import { setTasks } from './actionCreators';
 import '../styles/TaskForm.css';
-const schema = yup.object({
-	description: yup.string().required('Description is required'),
-});
-function ContactForm({ setTasks }) {
-	const handleSubmit = async (evt) => {
-		const isValid = await schema.validate(evt);
-		if (!isValid) {
-			return;
-		}
-		await addTask(evt);
-		const response = await getTasks();
-		setTasks(response.data);
-	}
-	return (
-		<div className="form">
-			<Formik
-				validationSchema={schema}
-				onSubmit={handleSubmit}
-			>
-				{({
-					  handleSubmit,
-					  handleChange,
-					  handleBlur,
-					  values,
-					  touched,
-					  isInvalid,
-					  errors,
-				  }) => (
-					<Form noValidate onSubmit={handleSubmit}>
-						<Form.Row>
-							<Form.Group as={Col} md="12" controlId="firstName">
-								<Form.Label>
-									<h4>Add Task</h4>
-								</Form.Label>
-								<Form.Control
-									type="text"
-									name="description"
-									placeholder="Task Description"
-									value={values.description || ''}
-									onChange={handleChange}
-									isInvalid={touched.description && errors.description}
-								/>
-								<Form.Control.Feedback type="invalid">
-									{errors.description}
-								</Form.Control.Feedback>
-							</Form.Group>
-						</Form.Row>
-						<Button type="submit" style={{ 'marginRight': '10px' }}>Save</Button>
-					</Form>
+import divWithClassName from "react-bootstrap/cjs/divWithClassName";
+import ErrorBoundary from "react-beautiful-dnd/src/view/drag-drop-context/error-boundary";
+
+
+const initialValues = {
+	task: '',
+	image: '',
+	dueDate: '',
+	reward: '',
+	steps: [
+		{
+			content: '',
+		},
+	],
+};
+
+export const TaskForm = () => (
+	<div>
+		<Formik
+			initialValues={initialValues}
+			validationSchema={Yup.object({
+				task: Yup.string().required('Required'),
+				image: Yup.mixed(),
+				dueDate: Yup.date,
+				reward: Yup.string(),
+				steps: Yup.array().of(
+					Yup.object({
+						content: Yup.string(),
+					})
+				),
+			})}
+			onSubmit={values => {
+				setTimeout(() => {
+					alert(JSON.stringify(values, null, 2));
+				}, 500);
+			}}
+		>
+			{({ values, isSubmitting }) => (
+				<Form>
+					<Field name="task" type="text" placeholder"Task" />
+					<ErrorMessage name="task">
+						{msg => <div className="field-error">{msg}</div>}
+					</ErrorMessage>
+					<div className="row">
+						<div className="col">
+							<Field name="image" type="image" />
+						</div>
+						<div className="col">
+							<Field name="dueDate" type="dateTime"  />
+						</div>
+						<div className="col">
+							<Field name="reward" type="text" />
+						</div>
+					</div>
+					<FieldArray name="steps">
+						{({ push, remove }) =>
+					<React.Fragment>
+						{values.steps && values.steps.length > 0 && values.steps.map((step, index) =>
+						<div className="row">
+							<div className="col">
+								<Field name={`steps[${index}].content`} type="text" placeholder"Add a step" />
+							</div>
+							<div className="col">
+								<button type="button" onClick={() => remove(index)}>X</button>
+							</div>
+						</div>
+						)}
+					<button
+						type="button"
+						onClick={() => push({content: ''})}
+						className="secondary"
+					>
+						Add Step
+					</button>
+					</React.Fragment>
+						}
+					</FieldArray>
+					<button type="submit" disabled={isSubmitting}>Create Task</button>
+					<Debug />
+				</Form>
 				)}
-			</Formik>
-		</div>
-	);
-}
-ContactForm.propTypes = {
-}
-const mapStateToProps = state => {
-	return {
-		tasks: state.tasks,
-	}
-}
-const mapDispatchToProps = dispatch => ({
-	setTasks: tasks => dispatch(setTasks(tasks))
-})
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(ContactForm);
+		</Formik>
+	</div>
+); // end of TaskForm
