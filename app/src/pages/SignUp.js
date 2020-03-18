5ed31a6d3c673862620ba48e283eb6112602cf35
Jacob Lott <58454146+jlott1999@users.noreply.github.com>
2020-03-18 17:31:31 -0600

    Made sign up page scrollable

--- app/src/pages/SignUp.js
+++ app/src/pages/SignUp.js
@@ -10,21 +10,27 @@ export const SignUp = () => {
     return (
         <>
             <Header/>
-            <div content="Spacing">
-                <div className="container">
-                    <div className="col-lg-5  mx-auto mt-5">
-                        <div className="card w-lg-50">
-                            <div className="row my-4">
-                                <div className="card-body">
-                                <h5 className="card-title">Register For An Account</h5>
-                                <SignUpForm/>
+            <div className="container">
+                <div content="Spacing">
+                    <div className="container">
+                        <div className="col-lg-5  mx-auto mt-5">
+                            <div className="card w-lg-50">
+                                <div className="row my-4">
+                                    <div className="card-body">
+                                    <h5 className="card-title">Register For An Account</h5>
+                                    <SignUpForm/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className="row sign-in-margin">
+                </div>
+            <div className="row">
+                <Footer/>
             </div>
-            <Footer/>
+            </div>
         </>
     )
 };
