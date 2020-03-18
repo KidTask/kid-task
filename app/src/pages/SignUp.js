3d1ed70dc805ce105a0993df9ebeb648e2a099a3
Jacob Lott <58454146+jlott1999@users.noreply.github.com>
2020-03-18 16:59:24 -0600

    Made Sign Up Form smaller and centered

--- app/src/pages/SignUp.js
+++ app/src/pages/SignUp.js
@@ -12,10 +12,16 @@ export const SignUp = () => {
             <Header/>
             <div content="Spacing">
                 <div className="container">
-                    <div className="row my-4">
-                        <h5 className="card-title">Register For An Account</h5>
+                    <div className="col-lg-5  mx-auto mt-5">
+                        <div className="card w-lg-50">
+                            <div className="row my-4">
+                                <div className="card-body">
+                                <h5 className="card-title">Register For An Account</h5>
+                                <SignUpForm/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <SignUpForm/>
                 </div>
             </div>
             <Footer/>
