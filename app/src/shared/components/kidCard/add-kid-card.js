aabb1a70c9551f49b8883940506378b69fb24205
Demetria Gutierrez <accounts@calyx.studio>
2020-03-26 14:15:37 -0600

    Linked kid sign-up from adult dashboard (using history).

--- app/src/shared/components/kidCard/add-kid-card.js
+++ app/src/shared/components/kidCard/add-kid-card.js
@@ -1,15 +1,23 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import React from "react";
+import {useHistory} from "react-router";
+
+
+
 
 export const AddKidCard = () => {
+	const history = useHistory();
+	console.log(history);
 	return (
 		<>
 			<Card border="secondary" text="secondary">
 				<Card.Img variant="top" src="http://placeholder.pics/svg/284x196" />
 				<Card.Body className="text-center">
 					<Card.Title>New Kid</Card.Title>
-					<Button variant="outline-secondary" href="/kid-sign-up">+ Add Kid</Button>
+					<Button variant="outline-secondary" onClick={()=>
+						history.push("/kid-sign-up")
+					}>+ Add Kid</Button>
 				</Card.Body>
 			</Card>
 		</>
