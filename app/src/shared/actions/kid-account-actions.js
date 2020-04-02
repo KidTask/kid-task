613b5083073667bd04dcd02f3312ae0eb73d0c6a
Demetria Gutierrez <accounts@calyx.studio>
2020-04-02 15:00:38 -0600

    Deleted unused signup modals

--- app/src/shared/actions/kid-account-actions.js
+++ app/src/shared/actions/kid-account-actions.js
@@ -9,3 +9,7 @@ export const getKidByKidAdultId = (kidAdultId) => async (dispatch) => {
 	const {data} = await httpConfig(`/apis/kid-account/?kidAdultId=${kidAdultId}`);
 	dispatch({ type: 'GET_KID_BY_KID_ADULT_ID', payload: data })
 };
+export const getKidName = () => async (dispatch) => {
+	const {data} = await httpConfig('/apis/kid-account/?kidUsername=${kidName}');
+	dispatch({ type: 'GET_KID_USERNAME', payload: data })
+};
